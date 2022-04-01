1f42a55603e3e1a503285027b01d6c3d32befc8f
bengrr <98470500+bengrr@users.noreply.github.com>
2022-03-31 20:10:43 -0400

    made my editgit add simplemath.py :)

--- simplemath.py
+++ simplemath.py
@@ -7,3 +7,16 @@ def square_area(length, width):
 """print apple"""
 
 print("apple")
+
+"""Ben's Edit, algebra"""
+
+x = 5
+y = 3
+
+answerx = input("What is x if y = 2x - 7? ")
+answery = input("What is y if y = 2x - 7? ")
+
+if float(answerx) == 5 and float(answery) == 3:
+    print("correct!")
+else:
+    print("wrong :(")
