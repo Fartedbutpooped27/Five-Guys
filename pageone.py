2571e39dabf796160c7955b50bcc343ef8d52a26
bengrr <98470500+bengrr@users.noreply.github.com>
2022-04-14 18:42:03 -0400

    Update pageone.py

--- pageone.py
+++ pageone.py
@@ -1,39 +1,13 @@
-#python code for page one
+"""Skeleton of our program"""
 
-CARDS = {
-    "two": 2,
-    "three": 3,
-    "four": 4,
-    "five": 5,
-    "six": 6,
-    "seven": 7,
-    "eight": 8,
-    "nine": 9,
-    "ten": 10,
-    "Jack": 11,
-    "Queen": 12,
-    "King": 13,
-    "Ace": 14,
-    "Joker": 15
-}
-
-class Pageone():
-    """ """
-    def __init__(self, player):
+class PageOne:
+    def __init__(self):
         return None
-        
-class Player():
-    """ """
-    def __init__(self, player_name):
-        """ """
-        self.player_name = player_name   
     
-class Human(Player):
-    """ """
-    def take_turn(self, hand):
-        """ """
-        
+class Player:
+    def __init__(self):
+        return None
+    
 class Computer(Player):
-    """ """
-    def take_turn(self, hand):
-        """ """
+    def __init__(self):
+        return None
