46aba0fa5a99aceaeaa2b61bbfc9c96872b99173
Jay Yoon <jyoon111@terpmail.umd.edu>
2022-04-14 17:14:17 -0400

    start

---
+++ pageone.py
@@ -0,0 +1,27 @@
+"""An Implementation of the game Page One"""
+
+CARDS = {
+    "two": 2,
+    "three": 3,
+    "four": 4,
+    "five": 5,
+    "six": 6,
+    "seven": 7,
+    "eight": 8,
+    "nine": 9,
+    "ten": 10,
+    "Jack": 11,
+    "Queen": 12,
+    "king": 13,
+    "Ace": 14,
+    "Joker": 15,
+    
+}
+class Player:
+    def __init__(self, player):
+        
+    def gameover(self):
+        
+    def playround(self):
+        
+        
